ef8881000db5e8f1765dd1954f792312abb7f4a8
allisonmazurek <60490768+allisonmazurek@users.noreply.github.com>
2022-11-05 13:40:09 -0400

    Project Structure Redone

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,18 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  #ifdef POWER
+    // Call POWER setup function
+  #endif
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  #ifdef POWER
+    // Call POWER setup function
+  #endif
+
   int systemCurrent = getSystemCurrent();                           //Read System Current
-  
   int thruster1_current = getThrusterCurrent().thruster_1_current;  //Read Current From Thruster #1 Example
 }
